c74db4538a3848611eb6c737c8dfeb7e724394c1
Miaritiana <hei.miaritiana@gmail.com>
2022-12-12 11:52:54 +0300

    feat: remove unused import

--- src/main/java/app/prog/service/BookService.java
+++ src/main/java/app/prog/service/BookService.java
@@ -4,7 +4,6 @@ import app.prog.model.BookEntity;
 import app.prog.repository.BookRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -30,27 +29,11 @@ public class BookService {
 
     //TODO-3: should I use Integer here or int ? Why ?
     public BookEntity deleteBook(int BookEntityId) {
-        /*
-        TIPS: From the API, the Class Optional<T> is :
-        A container object which may or may not contain a non-null value.
-        If a value is present, isPresent() returns true.
-        If no value is present, the object is considered empty and isPresent() returns false.
-
-        T is the type of the value, for example : here the class type is BookEntity
-         */
         Optional<BookEntity> optional = repository.findById(String.valueOf(BookEntityId));
         if (optional.isPresent()) {
             repository.delete(optional.get());
             return optional.get();
         } else {
-        /*
-        TODO-5 : The exception appears as an internal server error, status 500.
-        We all know that the appropriate error status is the 404 Not Found.
-        Any solution to do this ?
-        These links may help you :
-        Link 1 : https://www.baeldung.com/spring-response-entity
-        Link 2 : https://www.baeldung.com/exception-handling-for-rest-with-spring
-         */
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,
                     "Book "+ BookEntityId + " not found");
         }
