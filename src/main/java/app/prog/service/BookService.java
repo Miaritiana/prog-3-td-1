08130c5e9457774c6e0afce1fe6d4a3ba0e138c8
Miaritiana <hei.miaritiana@gmail.com>
2022-12-12 11:22:59 +0300

    feat: resolve TODO-5

--- src/main/java/app/prog/service/BookService.java
+++ src/main/java/app/prog/service/BookService.java
@@ -3,7 +3,10 @@ package app.prog.service;
 import app.prog.model.BookEntity;
 import app.prog.repository.BookRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,7 +51,8 @@ public class BookService {
         Link 1 : https://www.baeldung.com/spring-response-entity
         Link 2 : https://www.baeldung.com/exception-handling-for-rest-with-spring
          */
-            throw new RuntimeException("BookEntity." + BookEntityId + " not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Book "+ BookEntityId + "not found");
         }
     }
 }
